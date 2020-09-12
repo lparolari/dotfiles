72d0329ea75a3d969db9f196201f3bd5e3e89f41
lparolari <luca.parolari23@gmail.com>
2020-09-12 17:39:14 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,12 @@ function doIt() {
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
+		--exclude "apt.sh" \
+		--exclude "extra.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	echo "You may need to reopen the shell to get updates."
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
