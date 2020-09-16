e6b8443771d9abbde9559ed54acaf5b033b8d4dd
lparolari <luca.parolari23@gmail.com>
2020-09-16 14:23:08 +0200

    apt.sh: Add docker-compose install

--- apt.sh
+++ apt.sh
@@ -34,6 +34,10 @@ sudo apt install docker-ce
 sudo apt install docker-ce-cli 
 sudo apt install containerd.io
 
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Install awesome apps
 sudo snap install spotify
 
